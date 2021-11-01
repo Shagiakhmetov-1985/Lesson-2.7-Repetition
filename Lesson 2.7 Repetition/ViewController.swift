812d7278c606b5efb46a1a3cfdee31cf62d8d90e
Marat <Shagiakhmetov2499@gmail.com>
2021-11-01 13:24:56 +0300

    Initial Commit

---
+++ Lesson 2.7 Repetition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.7 Repetition
+//
+//  Created by Marat Shagiakhmetov on 01.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
